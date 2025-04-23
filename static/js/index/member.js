299235aab1fd4a5b40e8687f5bd668ba684a01cc
congluat03 <maihuynhcongluat@gmail.com>
2025-04-23 15:10:28 +0700

    Cập nhật giao diện ngày 23/4

---
+++ static/js/index/member.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById('modalDangKy').classList.remove('hidden');
+  }
+
+  function closeModal() {
+    document.getElementById('modalDangKy').classList.add('hidden');
+  }
